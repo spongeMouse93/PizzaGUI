3c0ae174caeb265659b0058a48c361086ea9be04
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2023-12-11 08:41:09 -0500

    Create module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module pizza.pizza {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens pizza.pizza to javafx.fxml;
+    exports pizza.pizza;
+}
